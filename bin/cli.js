d691bbdbe574d8129a46275a1e38076ee1dd0668
john <johnzhengyuliu@gmail.com>
2022-05-23 19:00:23 -0500

    npx

--- bin/cli.js
+++ bin/cli.js
@@ -13,7 +13,7 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/jamesrp13/solana-npx-client-template/ ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/Unboxed-Software/solana-npx-client-template ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository with name ${repoName}`)
