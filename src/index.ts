c190a5c26ea0f7f66dbb6b137d7069c8ab100e8c
John <75003086+ZYJLiu@users.noreply.github.com>
2022-08-24 15:21:16 -0500

    fix: update confirmTransaction (#1)

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 
 async function main() {
-    const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-    const user = await initializeKeypair(connection)
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
+  const user = await initializeKeypair(connection)
 
-    console.log(user.publicKey.toBase58())
+  console.log("PublicKey:", user.publicKey.toBase58())
 }
 
 main()
-    .then(() => {
-        console.log("Finished successfully")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log("Finished successfully")
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
